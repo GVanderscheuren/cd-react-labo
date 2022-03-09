be4032e6a9651af317ca33b9c78e4494361020b6
VanderschuerenGiany <giany.vanderschueren@student.howest.be>
2022-03-09 13:54:18 +0100

    End of class

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AppContainer from './components/AppContainer';
+import AppRow from './components/AppRow';
+import Header from './components/TodoAppHeader';
+import TodoInput from './components/TodoInput';
+import TodoItem from './components/TodoItem';
+import data from './data/data.json';
 
 function App() {
+  const [toDolist, setTodoList] = useState(data);
+
   return (
-    <p>Okay lets gooooooooooo</p>
+    <main className="App">
+      <AppContainer>
+        <Header />
+        <AppRow>
+          <TodoInput />
+          <TodoItem />
+        </AppRow>
+      </AppContainer>
+    </main>
   );
 }
 
